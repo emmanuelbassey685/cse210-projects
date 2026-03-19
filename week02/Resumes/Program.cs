7e1d3b9991acf4a596f5c00f0dd99430547b2799
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-03-19 07:25:46 +0300

    Add Journal Program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,18 +7,18 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2014;
-        job1._endYear = 2022;
+        job1._startYear = 2026;
+        job1._endYear = 2026;
 
         Job job2 = new Job();
         job2._jobTitle = "Data Analyst";
         job2._company = "Apple";
-        job2._startYear = 2014;
-        job2._endYear = 2024;
+        job2._startYear = 2026;
+        job2._endYear = 2026;
 
         // Creating a new instance of the Resume class
         Resume resume1 = new Resume();
-        resume1._name = "Gift Ndoma";
+        resume1._name = "Bassey, Emmanuel";
         resume1._jobs.Add(job1);
         resume1._jobs.Add(job2);
 
