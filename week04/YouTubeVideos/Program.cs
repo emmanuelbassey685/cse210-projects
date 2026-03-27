4c7a2a08c801e518c82f67608d68d8fafe96d673
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-03-27 21:20:41 +0300

    Add online ordering and Youtube video

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Officer Black Belt", "Kim Woo-bin", 6485);
+        Comment comment1 = new Comment("Bassey Emmanuel", "Highly Ineresting!");
+        Comment comment2 = new Comment("Bassey Emmanuel", "Highly Ineresting!!");
+        Comment comment3 = new Comment("Bassey Emmanuel", "Highly Ineresting!!!");
+        video1.AddComment(comment1);
+        video1.AddComment(comment2);
+        video1.AddComment(comment3);
+        videos.Add(video1);
+
+        Video video2 = new Video("The 8 Night", "Lee Sung-min", 6900);
+        Comment comment4 = new Comment("Bassey Emmanuel", "Highly Ineresting!");
+        Comment comment5 = new Comment("Bassey Emmanuel", "Highly Ineresting!!");
+        Comment comment6 = new Comment("Bassey Emmanuel", "Highly Ineresting!!!");
+        video2.AddComment(comment4);
+        video2.AddComment(comment5);
+        video2.AddComment(comment6);
+        videos.Add(video2);
+
+        Video video3 = new Video("The Point Men", "Hwang Jung-min", 6480);
+        Comment comment7 = new Comment("Bassey Emmanuel", "Highly Ineresting!");
+        Comment comment8 = new Comment("Bassey Emmanuel", "Highly Ineresting!!");
+        Comment comment9 = new Comment("Bassey Emmanuel", "Highly Ineresting!!!");
+        video3.AddComment(comment7);
+        video3.AddComment(comment8);
+        video3.AddComment(comment9);
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
