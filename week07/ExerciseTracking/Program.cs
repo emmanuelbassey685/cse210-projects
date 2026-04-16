092042a25bf5b0619b10e70f673c4badb177c926
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-04-16 04:33:13 +0300

    Add Exercise Tracking Program (Foundation#3)

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,26 @@
-using System;
+// Abstraction: Base class defines abstract methods
+//Encapsulation: Attributes are private or protected.
+//Inheritance: All activities share _date and _minutes.
+//Polymorphism: GetDistance(), GetSpeed(), and GetPace() are overridden in derived classes to provide specific calculations.
 
-class Program
+using System;
+using System.Collections.Generic;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        // Create one of each activity type
+        activities.Add(new Cycling(new DateTime(2026, 11, 03), 45, 15.0));
+        activities.Add(new Running(new DateTime(2025, 11, 04), 30, 3.0));
+        activities.Add(new Swimming(new DateTime(2024, 11, 05), 20, 20));
+
+        // Iterate and display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
